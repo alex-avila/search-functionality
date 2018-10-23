7685dff2f26868bd530be3fbd5807544fbae7708
Alex Avila <alex.avilx@icloud.com>
2018-10-23 13:37:08 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>search-functionality</h1>
+  </section>
+</template>
+
+<script>
+import products from '@/static/products.json'
+
+export default {
+  created() {
+    console.log(products[0])
+  }
+}
+</script>
+
+<style>
+</style>
